83747444794871d4e8369f0b1f1ce4f5a3611ef4
Hoang Loc <hoangphuocloc.phurieng@gmail.com>
2024-08-16 16:46:25 +0700

    update readme

--- src/configs/apis.js
+++ src/configs/apis.js
@@ -1,96 +1,58 @@
-export const API_FETCH_ALL_COMMON_QUESTIONS =
-  'http://localhost:4000/common-questions';
-export const API_FETCH_ALL_CUSTOMER_QUESTIONS =
-  'http://localhost:4000/customer-questions';
-export const API_CREATE_COMMON_QUESTIONS =
-  'http://localhost:4000/admin/common-questions/create';
-export const API_UPDATE_COMMON_QUESTIONS =
-  'http://localhost:4000/admin/common-questions/update';
-export const API_DELETE_COMMON_QUESTIONS =
-  'http://localhost:4000/admin/common-questions/delete';
+export const API_FETCH_ALL_COMMON_QUESTIONS = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/common-questions`;
+export const API_FETCH_ALL_CUSTOMER_QUESTIONS = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/customer-questions`;
+export const API_CREATE_COMMON_QUESTIONS = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/common-questions/create`;
+export const API_UPDATE_COMMON_QUESTIONS = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/common-questions/update`;
+export const API_DELETE_COMMON_QUESTIONS = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/common-questions/delete`;
 
-export const API_RESOLVE_CUSTOMER_QUESTIONS =
-  'http://localhost:4000/admin/customer-questions/resolve-question';
+export const API_RESOLVE_CUSTOMER_QUESTIONS = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/customer-questions/resolve-question`;
 
-export const API_SERIES = 'http://localhost:4000/film/series';
-export const API_MOVIES = 'http://localhost:4000/film/movies';
-export const API_CREATE_MOVIES =
-  'http://localhost:4000/admin/film/movies/create-movies';
-export const API_DELETE_MOVIES =
-  'http://localhost:4000/admin/film/movies/delete-movies';
-export const API_UPDATE_MOVIES =
-  'http://localhost:4000/admin/film/movies/update-movies';
-export const API_CREATE_SERIES =
-  'http://localhost:4000/admin/film/series/create-series';
-export const API_DELETE_SERIES =
-  'http://localhost:4000/admin/film/series/delete-series';
-export const API_UPDATE_SERIES =
-  'http://localhost:4000/admin/film/series/update-series';
-export const API_CATEGORY = 'http://localhost:4000/category';
-export const API_CREATE_CATEGORY =
-  'http://localhost:4000/admin/category/create-category';
-export const API_DELETE_CATEGORY =
-  'http://localhost:4000/admin/category/delete-category';
-export const API_UPDATE_CATEGORY =
-  'http://localhost:4000/admin/category/update-category';
-export const API_LOGIN = 'http://localhost:4000/admin/auth/login';
-export const API_GET_ALL_USER = 'http://localhost:4000/admin/manage/user';
-export const API_POST_CREATE_USER =
-  'http://localhost:4000/admin/manage/user/create';
-export const API_POST_UPDATE_USER =
-  'http://localhost:4000/admin/manage/user/update';
-export const API_GET_ALL_SUBSCRIBER =
-  'http://localhost:4000/admin/manage/subscriber';
-export const API_RESET_PASSWORD =
-  'http://localhost:4000/admin/auth/reset-password';
-export const API_DELETE_USER = 'http://localhost:4000/admin/auth/delete-user';
-export const API_ADD_MANY_SERIES =
-  'http://localhost:4000/admin/film/series/add-many-series';
-export const API_ADD_MANY_MOVIES =
-  'http://localhost:4000/admin/film/movies/add-many-movies';
-export const API_ADD_MANY_CATEGORY =
-  'http://localhost:4000/admin/category/add-many-category';
-export const API_GET_SUMMARY_REGISTER =
-  'http://localhost:4000/admin/summary/summary-register';
-export const API_GET_SUMMARY_PURCHASES =
-  'http://localhost:4000/admin/summary/summary-purchases';
-export const API_GET_SUMMARY_TOTAL_EACH_PACKAGE =
-  'http://localhost:4000/admin/summary/summary-total-each-package';
-export const API_GET_SUMMARY_PURCHASES_REGISTER_CURRENT_YEAR =
-  'http://localhost:4000/admin/summary/register-purchases-current-year';
-export const API_GET_SUBSCRIBER_ORDER_TODAY =
-  'http://localhost:4000/admin/manage/subscriber/subscriber-order-today';
-export const API_GET_SUBSCRIBER_TODAY =
-  'http://localhost:4000/admin/manage/subscriber/subscriber-today';
-export const API_GET_BANNED_SUBSCRIBER =
-  'http://localhost:4000/admin/manage/subscriber/banned-subscriber';
-export const API_GET_RECOVER_SUBSCRIBER =
-  'http://localhost:4000/admin/manage/subscriber/recover-subscriber';
-export const API_RECOVER_MOVIES =
-  'http://localhost:4000/admin/film/movies/recover-movies';
-export const API_RECOVER_SERIES =
-  'http://localhost:4000/admin/film/series/recover-series';
-export const API_SERIES_ADMIN = 'http://localhost:4000/admin/film/series';
-export const API_GET_DATA_PAYMENT = 'http://localhost:4000/payment';
-export const API_GET_DATA_PACKAGE = 'http://localhost:4000/package';
-export const API_GET_ADMIN_PACKAGE = 'http://localhost:4000/admin/package';
-export const API_GET_ADMIN_PAYMENT = 'http://localhost:4000/admin/payment';
-export const API_UPDATE_PROFILE =
-  'http://localhost:4000/auth/profile?user=user';
-export const API_CHANGE_PASSWORD =
-  'http://localhost:4000/auth/change-password?user=user';
-export const API_CHANGE_AVATAR =
-  'http://localhost:4000/auth/change-avatar?user=user';
-export const API_DELETE_AVATAR =
-  'http://localhost:4000/auth/delete-avatar?user=user';
-export const API_VERIFY_TOKEN = 'http://localhost:4000/auth/verify-token';
+export const API_SERIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/film/series`;
+export const API_MOVIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/film/movies`;
+export const API_CREATE_MOVIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/movies/create-movies`;
+export const API_DELETE_MOVIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/movies/delete-movies`;
+export const API_UPDATE_MOVIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/movies/update-movies`;
+export const API_CREATE_SERIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/series/create-series`;
+export const API_DELETE_SERIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/series/delete-series`;
+export const API_UPDATE_SERIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/series/update-series`;
+export const API_CATEGORY = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/category`;
+export const API_CREATE_CATEGORY = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/category/create-category`;
+export const API_DELETE_CATEGORY = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/category/delete-category`;
+export const API_UPDATE_CATEGORY = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/category/update-category`;
+export const API_LOGIN = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/auth/login`;
+export const API_GET_ALL_USER = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/manage/user`;
+export const API_POST_CREATE_USER = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/manage/user/create`;
+export const API_POST_UPDATE_USER = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/manage/user/update`;
+export const API_GET_ALL_SUBSCRIBER = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/manage/subscriber`;
+export const API_RESET_PASSWORD = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/auth/reset-password`;
+export const API_DELETE_USER = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/auth/delete-user`;
+export const API_ADD_MANY_SERIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/series/add-many-series`;
+export const API_ADD_MANY_MOVIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/movies/add-many-movies`;
+export const API_ADD_MANY_CATEGORY = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/category/add-many-category`;
+export const API_GET_SUMMARY_REGISTER = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/summary/summary-register`;
+export const API_GET_SUMMARY_PURCHASES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/summary/summary-purchases`;
+export const API_GET_SUMMARY_TOTAL_EACH_PACKAGE = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/summary/summary-total-each-package`;
+export const API_GET_SUMMARY_PURCHASES_REGISTER_CURRENT_YEAR = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/summary/register-purchases-current-year`;
+export const API_GET_SUBSCRIBER_ORDER_TODAY = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/manage/subscriber/subscriber-order-today`;
+export const API_GET_SUBSCRIBER_TODAY = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/manage/subscriber/subscriber-today`;
+export const API_GET_BANNED_SUBSCRIBER = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/manage/subscriber/banned-subscriber`;
+export const API_GET_RECOVER_SUBSCRIBER = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/manage/subscriber/recover-subscriber`;
+export const API_RECOVER_MOVIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/movies/recover-movies`;
+export const API_RECOVER_SERIES = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/series/recover-series`;
+export const API_SERIES_ADMIN = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/film/series`;
+export const API_GET_DATA_PAYMENT = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/payment`;
+export const API_GET_DATA_PACKAGE = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/package`;
+export const API_GET_ADMIN_PACKAGE = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/package`;
+export const API_GET_ADMIN_PAYMENT = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/payment`;
+export const API_UPDATE_PROFILE = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/auth/profile?user=user`;
+export const API_CHANGE_PASSWORD = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/auth/change-password?user=user`;
+export const API_CHANGE_AVATAR = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/auth/change-avatar?user=user`;
+export const API_DELETE_AVATAR = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/auth/delete-avatar?user=user`;
+export const API_VERIFY_TOKEN = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/auth/verify-token`;
 
-export const API_CREATE_MESSAGE = 'http://localhost:4000/admin/message/create';
-export const API_UPDATE_MESSAGE = 'http://localhost:4000/admin/message/update';
-export const API_UPDATE_OFF_MESSAGE =
-  'http://localhost:4000/admin/message/update-off';
+export const API_CREATE_MESSAGE = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/message/create`;
+export const API_UPDATE_MESSAGE = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/message/update`;
+export const API_UPDATE_OFF_MESSAGE = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/message/update-off`;
 
-export const API_UPDATE_JOIN_MESSAGE =
-  'http://localhost:4000/admin/message/join-chat';
+export const API_UPDATE_JOIN_MESSAGE = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/admin/message/join-chat`;
 
-export const API_GET_ON_MESSAGE = 'http://localhost:4000/message/get-on';
+export const API_GET_ON_MESSAGE = `${process.env.REACT_APP_PUBLIC_HOST_BACKEND}/message/get-on`;
